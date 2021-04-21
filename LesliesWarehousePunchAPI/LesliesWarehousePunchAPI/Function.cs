3f0399c4abfe4a14a2dc27c5f6b0e236f1b3b0ec
JaclynFox <54548987+JaclynFox@users.noreply.github.com>
2021-04-20 23:13:12 -0500

    Punch API created
    The punch API has been created. TODO: testing and refactoring the employee login and management API to fit newest database format. Testing application will be created tomorrow.

--- LesliesWarehousePunchAPI/LesliesWarehousePunchAPI/Function.cs
+++ LesliesWarehousePunchAPI/LesliesWarehousePunchAPI/Function.cs
@@ -5,7 +5,7 @@ using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
-using Newtonsoft.Json;
+using System.Globalization;
 
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
@@ -18,10 +18,10 @@ namespace LesliesWarehousePunchAPI
         public async Task<List<PunchRecord>> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
         {
             Dictionary<string, string> iparams = (Dictionary<string, string>)input.QueryStringParameters;
-            string type = string.Empty, pid = string.Empty, empID = string.Empty, date = string.Empty, pType = string.Empty;
-            iparams.TryGetValue("type", out type);
+            string request = string.Empty, pid = string.Empty, empID = string.Empty, date = string.Empty, pType = string.Empty;
+            iparams.TryGetValue("request", out request);
             List<PunchRecord> punches = new List<PunchRecord>();
-            switch (type)
+            switch (request)
             {
                 case "admin":
                     iparams.TryGetValue("date", out date);
@@ -32,12 +32,12 @@ namespace LesliesWarehousePunchAPI
                     iparams.TryGetValue("empID", out empID);
                     iparams.TryGetValue("date", out date);
                     iparams.TryGetValue("pType", out pType);
-                    await UpdatePunch(int.Parse(pid), int.Parse(empID), date, pType);
+                    UpdatePunch(pid, empID, date, pType);
                     break;
                 default:
                     iparams.TryGetValue("empID", out empID);
                     iparams.TryGetValue("pType", out pType);
-                    await Punch(empID, pType);
+                    Punch(empID, pType);
                     break;
             }
             return punches;
@@ -60,29 +60,87 @@ namespace LesliesWarehousePunchAPI
                 QueryResponse res = await client.QueryAsync(req);
                 foreach (Dictionary<String, AttributeValue> item in res.Items)
                 {
-                    punches.Add(new PunchRecord(int.Parse(item["punchID"].N.ToString()), int.Parse(item["empID"].N.ToString()), item["date"].S, item["punchType"].S, item["flag"].S));
+                    punches.Add(new PunchRecord(item["punchID"].S, item["empID"].S, item["date"].S, item["punchType"].S, item["flag"].S));
                 }
             } while (lastKeyEvaluated != null && lastKeyEvaluated.Count != 0);
             return punches;
         }
-        public async void UpdatePunch(int punchID, int empID, string date, string punchType)
+        public async void UpdatePunch(string punchID, string empID, string date, string punchType)
         {
             Dictionary<string, AttributeValue > attr = new Dictionary<string, AttributeValue>();
-            attr["punchID"] = new AttributeValue { N = punchID.ToString() };
-            attr["empID"] = new AttributeValue { N = empID.ToString() };
+            attr["punchID"] = new AttributeValue { S = punchID };
+            attr["empID"] = new AttributeValue { S = empID };
             attr["date"] = new AttributeValue { S = date };
             attr["punchType"] = new AttributeValue { S = punchType };
             attr["flag"] = new AttributeValue { S = await CheckForFlag(empID, date, punchType) };
             PutItemRequest req = new PutItemRequest(table, attr);
             PutItemResponse res = await client.PutItemAsync(req);
         }
-        public async void Punch(int empID, string punchType)
+        public async void Punch(string empID, string punchType)
         {
-            //todo code
+            string datestring = DateTime.Now.ToString("G", CultureInfo.CreateSpecificCulture("en-us"));
+            Dictionary<string, AttributeValue> attr = new Dictionary<string, AttributeValue>();
+            attr["punchID"] = new AttributeValue { S = empID + " " + datestring };
+            attr["empID"] = new AttributeValue { S = empID };
+            attr["date"] = new AttributeValue { S = datestring };
+            attr["punchType"] = new AttributeValue { S = punchType };
+            attr["flag"] = new AttributeValue { S = await CheckForFlag(empID, datestring, punchType) };
+            PutItemRequest req = new PutItemRequest(table, attr);
+            PutItemResponse res = await client.PutItemAsync(req);
         }
-        public async Task<string> CheckForFlag(int empID, string date, string punchType)
+        public async Task<string> CheckForFlag(string empID, string date, string punchType)
         {
-
+            string flag = string.Empty;
+            Dictionary<string, AttributeValue> lastKeyEvaluated = null;
+            List<PunchRecord> punches = new List<PunchRecord>();
+            do
+            {
+                QueryRequest req = new QueryRequest
+                {
+                    TableName = table,
+                    Limit = 10,
+                    KeyConditionExpression = "date < :v_date",
+                    ExpressionAttributeValues = new Dictionary<string, AttributeValue> {
+                        {":v_date", new AttributeValue { S =  date }}},
+                    ExclusiveStartKey = lastKeyEvaluated
+                };
+                QueryResponse res = await client.QueryAsync(req);
+                foreach (Dictionary<String, AttributeValue> item in res.Items)
+                {
+                    punches.Add(new PunchRecord(item["punchID"].S, item["empID"].S, item["date"].S, item["punchType"].S, item["flag"].S));
+                }
+            } while (lastKeyEvaluated != null && lastKeyEvaluated.Count != 0);
+            switch (punches[punches.Count - 1].PunchType)
+            {
+                case "out":
+                    if (punchType == "in" || punchType == "lunchin")
+                        flag = "no";
+                    else
+                        flag = "yes";
+                    break;
+                case "in":
+                    if (punchType == "out")
+                        flag = "no";
+                    else
+                        flag = "yes";
+                    break;
+                case "lunchout":
+                    if (punchType == "in")
+                        flag = "no";
+                    else
+                        flag = "yes";
+                    break;
+                case "lunchin":
+                    if (punchType == "lunchout")
+                        flag = "no";
+                    else
+                        flag = "yes";
+                    break;
+                default:
+                    flag = "yes";
+                    break;
+            }
+            return flag;
         }
     }
 }
--- LesliesWarehousePunchAPI/LesliesWarehousePunchAPI/PunchRecord.cs
+++ LesliesWarehousePunchAPI/LesliesWarehousePunchAPI/PunchRecord.cs
@@ -7,20 +7,19 @@ namespace LesliesWarehousePunchAPI
 {
     public class PunchRecord
     {
-        private int _punchID, _empID;
-        private string _datetime, _punchtype, _flag;
-        public int PunchID { get => _punchID; set => _punchID = value; }
-        public int EmpID { get => _empID; set => _empID = value; }
+        private string _punchID, _empID, _datetime, _punchType, _flag;
+        public string PunchID { get => _punchID; set => _punchID = value; }
+        public string EmpID { get => _empID; set => _empID = value; }
         public string Datetime { get => _datetime; set => _datetime = value; }
-        public string Punchtype { get => _punchtype; set => _punchtype = value; }
+        public string PunchType { get => _punchType; set => _punchType = value; }
         public string Flag { get => _flag; set => _flag = value; }
         [JsonConstructor]
-        public PunchRecord(int pid, int e, string d, string p, string f)
+        public PunchRecord(string pid, string e, string d, string p, string f)
         {
             PunchID = pid;
             EmpID = e;
             Datetime = d;
-            Punchtype = p;
+            PunchType = p;
             Flag = f;
         }
         public PunchRecord() { }
